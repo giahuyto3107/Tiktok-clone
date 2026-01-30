9382e3d39546d9e1b3a040577369e6b958bfbec6
giahuyto3107 <giahuyto3107@gmail.com>
2026-01-30 20:21:23 +0700

    fix (firebase): fix firebase import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,13 +79,13 @@ dependencies {
     implementation(libs.androidx.camera.view)
 
     // Import the Firebase BoM
-    implementation(libs.firebase.bom)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
 
 
     // TODO: Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation(libs.firebase.analytics)
-
+    implementation("com.google.firebase:firebase-auth")
 
     // Add the dependencies for any other desired Firebase products
     // https://firebase.google.com/docs/android/setup#available-libraries
