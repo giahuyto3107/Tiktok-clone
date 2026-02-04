b98afcbd5e118ff3f21bcd4882a8d1d6c1be85e0
sÒwÓs <115623421+NhatNam15151515@users.noreply.github.com>
2026-02-04 13:13:52 +0700

    Add and update fake data for social options and comments

--- app/src/main/java/com/example/tiktok_clone/features/social/fakeData/FakeCommentData.kt
+++ app/src/main/java/com/example/tiktok_clone/features/social/fakeData/FakeCommentData.kt
@@ -9,8 +9,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u1",
             userName = "Trùm Đẩy Deadline",
-            commentTime = "10-02-2025",
-            AvatarUrl = "https://i.pravatar.cc/150?img=1",
+            createdAt = 1739145600000L, // 10-02-2025
+            avatarUrl = "https://i.pravatar.cc/150?img=1",
             commentContent = "Video cuốn thật sự 👍",
             likeCount = 120,
             replyCount = 8
@@ -20,8 +20,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u2",
             userName = "Cha đẻ Tiktok",
-            commentTime = "10-02-2026",
-            AvatarUrl = "https://i.pravatar.cc/150?img=2",
+            createdAt = 1762992000000L, // 10-02-2026
+            avatarUrl = "https://i.pravatar.cc/150?img=2",
             commentContent = "Cười mà xem hoài",
             likeCount = 45,
             replyCount = 2
@@ -31,8 +31,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u3",
             userName = "Code Không Bug",
-            commentTime = "10-11-2025",
-            AvatarUrl = "https://i.pravatar.cc/150?img=3",
+            createdAt = 1762732800000L, // 10-11-2025
+            avatarUrl = "https://i.pravatar.cc/150?img=3",
             commentContent = "Đoạn cuối hơi nhanh, giá mà chậm lại chút",
             likeCount = 12,
             replyCount = 1
@@ -42,8 +42,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u4",
             userName = "Chúa Tể Ngủ Trễ",
-            commentTime = "10-12-2025",
-            AvatarUrl = "https://i.pravatar.cc/150?img=4",
+            createdAt = 1765411200000L, // 10-12-2025
+            avatarUrl = "https://i.pravatar.cc/150?img=4",
             commentContent = "Ủa rồi sao nữa 🤔",
             likeCount = 3,
             replyCount = 0
@@ -53,8 +53,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u5",
             userName = "Ông Hoàng Dark Mode",
-            commentTime = "8-1-2025",
-            AvatarUrl = "https://i.pravatar.cc/150?img=5",
+            createdAt = 1736448000000L, // 08-01-2025
+            avatarUrl = "https://i.pravatar.cc/150?img=5",
             commentContent = "Edit mượt ghê",
             isLiked = true,
             likeCount = 89,
@@ -65,8 +65,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u6",
             userName = "Chuông kì bu",
-            commentTime = "10-02-2025",
-            AvatarUrl = "",
+            createdAt = 1739145600000L, // 10-02-2025
+            avatarUrl = "",
             commentContent = "Xem giải trí ổn áp",
             likeCount = 0,
             replyCount = 0
@@ -76,9 +76,9 @@ object FakeCommentData {
             postId = "p1",
             userId = "u7",
             userName = "Tay Chơi Loli",
-            commentTime = "8-02-2025",
-            AvatarUrl = "https://i.pravatar.cc/150?img=6",
-            commentContent = "Xem mà tim đập nhanh hơn cả deadlinsadsadsadsadae 😭",
+            createdAt = 1738886400000L, // 08-02-2025
+            avatarUrl = "https://i.pravatar.cc/150?img=6",
+            commentContent = "Xem mà tim đập nhanh hơn deadline 😭",
             likeCount = 666,
             replyCount = 69611
         ),
@@ -87,8 +87,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u8",
             userName = "Xe Lăng Thần Tốc",
-            commentTime = "9-02-2025",
-            AvatarUrl = "https://i.pravatar.cc/150?img=7",
+            createdAt = 1738972800000L, // 09-02-2025
+            avatarUrl = "https://i.pravatar.cc/150?img=7",
             commentContent = "Tua chưa kịp là hết, coi lại lần 3 luôn 🚀",
             isLiked = true,
             likeCount = 314,
@@ -99,8 +99,8 @@ object FakeCommentData {
             postId = "p1",
             userId = "u9",
             userName = "Hoa Thanh Quế",
-            commentTime = "2 giờ",
-            AvatarUrl = "https://i.pravatar.cc/150?img=8",
+            createdAt = 1739232000000L, // 11-02-2025 (giả lập "2 giờ trước")
+            avatarUrl = "https://i.pravatar.cc/150?img=8",
             commentContent = "Nói rằng mình là người Thanh H..",
             likeCount = 101,
             replyCount = 4
