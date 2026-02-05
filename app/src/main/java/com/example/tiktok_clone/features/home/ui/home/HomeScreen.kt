dbb2579eb8a4bf6c3b723a1812de6d0aa4c55f48
sÒwÓs <115623421+NhatNam15151515@users.noreply.github.com>
2026-02-05 22:58:39 +0700

    feat(home): refine home screen UI and helper logic

--- app/src/main/java/com/example/tiktok_clone/features/home/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/tiktok_clone/features/home/ui/home/HomeScreen.kt
@@ -19,6 +19,9 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,29 +30,40 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.example.tiktok_clone.core.navigation.AppNavigation
 import com.example.tiktok_clone.R
+import com.example.tiktok_clone.features.social.model.Post
+import com.example.tiktok_clone.features.social.viewModel.SocialViewModel
 
 @Composable
 fun HomeScreen(
     onSearchTap: () -> Unit = {}
 ) {
-    val images = remember {
-        listOf(
-            R.drawable.apartment,
-            R.drawable.cat,
-            R.drawable.cherry_flower,
-            R.drawable.city_post_office,
-            R.drawable.independence_palace,
-            R.drawable.river,
-            R.drawable.road,
-            R.drawable.street,
-            R.drawable.uni,
-            R.drawable.video,
-        )
+//    val images = remember {
+//        listOf(
+//            R.drawable.apartment,
+//            R.drawable.cat,
+//            R.drawable.cherry_flower,
+//            R.drawable.city_post_office,
+//            R.drawable.independence_palace,
+//            R.drawable.river,
+//            R.drawable.road,
+//            R.drawable.street,
+//            R.drawable.uni,
+//            R.drawable.video,
+//        )
+//    }
+    val viewModel: SocialViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
+    LaunchedEffect(Unit) {
+        viewModel.loadPosts()
     }
-
-    val pagerState = rememberPagerState(pageCount = { images.size })
+    //lấy dữ liệu post ừ fake data
+    val posts = uiState.posts
+    val pagerState = rememberPagerState(pageCount = { posts.size})
 
     Box(modifier = Modifier.fillMaxSize()) {
         VerticalPager(
@@ -57,10 +71,14 @@ fun HomeScreen(
             modifier = Modifier.fillMaxSize()
         ) { page ->
             Column {
-                Box(modifier = Modifier.fillMaxHeight(0.95f)) {
-                    VideoSection(imageRes = images[page])
+                Box(modifier = Modifier
+                    .fillMaxHeight()) {
+                    VideoSection(thumbnailUrl = posts[page].thumbnailUrl)
 
                     MiddleSection(
+                        currentUserId = "u1", // lấy từ user login, chưa có user login nên gán tạm
+                        viewModel = viewModel,
+                        currentPost = posts[page],
                         modifier = Modifier
                             .align(Alignment.BottomEnd)
                             .padding(
@@ -68,15 +86,14 @@ fun HomeScreen(
                                 end = dimensionResource(R.dimen.spacing_m)
                             )
                     )
+                    VideoDescriptionSection(
+                        userName = posts[page].author.userName,
+                        description = posts[page].description,
+                        modifier = Modifier
+                            .padding(start = dimensionResource(R.dimen.spacing_m),)
+                            .align(Alignment.BottomStart)
+                    )
                 }
-
-                VideoDescriptionSection(
-                    userName = "User ${page + 1}",
-                    modifier = Modifier
-                        .padding(
-                            start = dimensionResource(R.dimen.spacing_m),
-                        )
-                )
             }
         }
 
@@ -116,14 +133,14 @@ private fun TopHeading(
         )
     }
 }
-
 @Composable
-private fun VideoSection(
-    @DrawableRes imageRes: Int,
+fun VideoSection(
+    thumbnailUrl: String?,
     modifier: Modifier = Modifier
 ) {
-    Image(
-        painter = painterResource(id = imageRes),
+    // cập nhật lấy ảnh từ url
+    AsyncImage(
+        model = thumbnailUrl,
         contentDescription = "Video Thumbnail",
         modifier = modifier.fillMaxSize(),
         contentScale = ContentScale.Crop
