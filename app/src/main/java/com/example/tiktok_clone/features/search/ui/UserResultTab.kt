dd36a213b37fcaf84ce5e0db66480991dfe951aa
giahuyto3107 <giahuyto3107@gmail.com>
2026-04-10 17:06:13 +0700

    chore: remove unused imports

--- app/src/main/java/com/example/tiktok_clone/features/search/ui/UserResultTab.kt
+++ app/src/main/java/com/example/tiktok_clone/features/search/ui/UserResultTab.kt
@@ -1,7 +1,5 @@
 package com.example.tiktok_clone.features.search.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,11 +9,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -25,13 +21,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.SubcomposeAsyncImage
 import com.example.tiktok_clone.features.profile.viewmodel.ProfileViewModel
 import com.example.tiktok_clone.features.search.model.UserItem
 import com.example.tiktok_clone.features.social.data.model.SocialAction
